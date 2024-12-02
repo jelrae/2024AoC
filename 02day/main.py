53cb618441d363306a424025a1b94442308fe015
Jordan Earle <43170680+jelrae@users.noreply.github.com>
2024-12-02 12:58:43 +0100

    Both working, clunky reports check function, could probably rewrite with recursion and or adding the one removed to the previous rather than this rechecking

--- 02day/main.py
+++ 02day/main.py
@@ -14,40 +14,51 @@ def load_file(fp='input.txt'):
     return d
 
 
-def determine_safety(fp = 'input.txt', exp_check = False, problem_dampener = True):
+def check_saftey(rep, ec = False, pd = False):
+    c = [rep[i] - rep[i + 1] for i in range(0, len(rep) - 1)]
+
+    scp = [1 <= i <= 3 for i in c]
+    scn = [-3 <= i <= -1 for i in c]
+
+    if ec:
+        check_output(rep, c, scp, scn, ec, pd)
+
+    return all(scp) or all(scn)
+
+
+def check_output(r, c, scp, scn, ec, pd):
+    if ec:
+        print('For the array :' + str(r))
+        print('This is the check array: ' + str(c))
+        if pd:
+            print('Nothing')
+        else:
+            print('Check for pos. good: ' + str(all(scp)))
+            print('Check for neg. good: ' + str(all(scn)))
+
+
+def determine_safety(fp = 'input.txt', check = False, exp_check = False, problem_dampener = False):
 
     reports = load_file(fp)
     rs = []
     for r in reports:
-        c = [r[i] - r[i+1] for i in range(0, len(r)-1)]
+        sc = check_saftey(r, exp_check, problem_dampener)
 
-        scp = [1 <= i <= 3 for i in c]
-        scn = [-3 <= i <= -1 for i in c]
-
-        if problem_dampener:
-            sc = np.sum(scp) >= len(scp) - 1 or np.sum(scn) >= len(scn) - 1
-        else:
-            sc = all(scp) or all(scn)
-
-        if exp_check:
-            print('For the array :' + str(r))
-            print('This is the check array: ' + str(c))
-            if problem_dampener:
-                print('Check for pos. good: ' + str(np.sum(scp) >= len(scp) - 1))
-                print('Check for neg. good: ' + str(np.sum(scn) >= len(scn) - 1))
-            else:
-                print('Check for pos. good: ' + str(all(scp)))
-                print('Check for neg. good: ' + str(all(scn)))
-        else:
+        if problem_dampener and not sc:
+            for i in range(0,len(r)):
+                r_new = r[:]
+                del r_new[i]
+                sc = check_saftey(r_new, exp_check, problem_dampener)
+                if sc:
+                    break
+        if check:
             print('For: ' + str(r) + ' it was determine that it was: ' + str(sc))
-
         rs.append(sc)
-    print(np.sum(rs))
-
+    print('We have found ' + str(np.sum(rs)) + ' safe levels')
 
 
 def main():
-    determine_safety('test_input.txt', True, True)
+    determine_safety('input.txt', False, False, True)
     # determine_safety('input.txt',False, False)
 
 
