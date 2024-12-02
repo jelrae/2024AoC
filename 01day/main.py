425b29f574f93a656c2aa4ce5be523cb7465c973
Jordan Earle <43170680+jelrae@users.noreply.github.com>
2024-12-02 11:28:46 +0100

    Added more to gitnore, finished day1

--- 01day/main.py
+++ 01day/main.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+from collections import Counter
 
 
 def load_file(fp='input.txt'):
@@ -7,7 +8,7 @@ def load_file(fp='input.txt'):
 
     with open('input.txt', 'r') as f:
         for line in f:
-            li = line.strip('\n').split('   ')
+            li = line.strip('\n').split()
             lc.append(li)
 
     lm = np.array([list(map(int, li)) for li in lc])
@@ -31,20 +32,42 @@ def determine_similarity():
     u0, c0 = np.array(np.unique(lm[:, 0], return_counts=True))
     u1, c1 = np.array(np.unique(lm[:, 1], return_counts=True))
 
-    s0 = set(u0)
-    s1 = set(u1)
-
-    print(s0 - s1)
-    print(s1 - s0)
-
-    # sl = set(np.sort(np.concatenate([c0, c1])))
+    si = set(u0) & set(u1)
 
     breakpoint()
+
+    d0 = {k: v for k, v in zip(u0, c0)}
+    d1 = {k: v for k, v in zip(u1, c1)}
+
+    sim_score = 0
+
+    for k in si:
+        ss = k * d1[k]
+        sim_score += ss
+
+    print("The sim score using dicts is: " + str(sim_score))
+
+
+def determine_similarity_counter():
+    lm = load_file()
+
+    c0 = Counter(lm[:, 0])
+    c1 = Counter(lm[:, 1])
+
+    sim_score = []
+
+    for k in set(c0.keys()) & set(c1.keys()):
+        sim_score.append(k * c1[k])
+
+    print(sim_score)
+
+    print("The sim score using counter is: " + str(np.sum(sim_score)))
 
 
 def main():
     determine_distance()
     determine_similarity()
+    determine_similarity_counter()
 
 
 if __name__ == "__main__":
