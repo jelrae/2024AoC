c980ee308cb99dd7a2d6e3b7da048f132c40d228
Jordan Earle <43170680+jelrae@users.noreply.github.com>
2024-12-03 09:42:25 +0100

    Day 3 first push, works on test input for part 1

---
+++ 03day/main.py
@@ -0,0 +1,35 @@
+import numpy as np
+import re
+
+def clean_text(line):
+    muls = re.findall("mul\(\d+,\d+\)", line)
+    mt = [re.findall('\d+,\d+', m)[0].split(',') for m in muls]
+    md = [list(map(int, m)) for m in mt]
+    return md
+
+
+def load_file(fp='input.txt'):
+    md = []
+
+    with open(fp, 'r') as f:
+        for line in f:
+            print(line)
+            mults = clean_text(line)
+            md += mults
+    print(md)
+    return md
+
+
+def determine_multiplications(fp = 'input.txt'):
+    srd = load_file(fp)
+    srd = np.array(srd)
+    breakpoint()
+    output = np.sum(srd[:,0] * srd[:,1])
+    print('The total of the mults in the system is : ' + str(output))
+
+def main():
+    fp = 'test_input.txt'
+    determine_multiplications(fp)
+
+if __name__ == "__main__":
+    main()
