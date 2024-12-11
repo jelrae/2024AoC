a6691d0593906c3f005d6cc5809f151d175bfc4a
Jordan Earle <43170680+jelrae@users.noreply.github.com>
2024-12-11 11:20:01 +0100

    Test 1 passed with previous version, but was too memory intensive. Thought of using a dict since the order doesnt matter and we just want the number of occurrences, much faster. Works on both

--- 11day/main11.py
+++ 11day/main11.py
@@ -1,6 +1,8 @@
 import numpy as np
 import re
 import math
+from collections import defaultdict
+
 
 # As you observe them for a while, you find that the stones have a consistent behavior. Every time you blink, the stones each simultaneously change according to the first applicable rule in this list:
 #
@@ -8,39 +10,48 @@ import math
 # If the stone is engraved with a number that has an even number of digits, it is replaced by two stones. The left half of the digits are engraved on the new left stone, and the right half of the digits are engraved on the new right stone. (The new numbers don't keep extra leading zeroes: 1000 would become stones 10 and 0.)
 # If none of the other rules apply, the stone is replaced by a new stsone; the old stone's number multiplied by 2024 is engraved on the new stone.
 
-def load_file(fp='input.txt'):
+class PillarLine:
+    def __init__(self, fp):
+        with open(fp, 'r') as f:
+            for line in f:
+                cl = line.split()
+                data = [int(i) if i.isnumeric() else np.nan for i in cl]
+        self.stones = {stone: data.count(stone) for stone in data}
 
-    with open(fp, 'r') as f:
-        for line in f:
-            cl = line.split()
-            data = [int(i) if i.isnumeric() else np.nan for i in cl]
+    def blink(self):
+        new_stones = defaultdict(int)
+        for stone in self.stones:
+            num_stones = self.stones[stone]
+            # Rule 1
+            if stone == 0:
+                new_stones[1] += num_stones
+            # Rule 2
+            elif (math.floor(np.log10(stone) + 1) % 2) == 0:
+                storage_stone = str(stone)
+                stone1, stone2 = [int(storage_stone[:int(len(storage_stone) / 2)]),
+                                  int(storage_stone[int(len(storage_stone) / 2):])]
+                new_stones[stone1] += num_stones
+                new_stones[stone2] += num_stones
+            # Rule 3
+            else:
+                new_stones[stone * 2024] += num_stones
+        self.stones = new_stones
 
-    return data
+    def rapid_blink(self, num_blinks):
+        for i in range(0, num_blinks):
+            self.blink()
+        return sum(self.stones.values())
 
-
-def blink(stones):
-
-    new_stones = []
-    for stone in stones:
-        # Rule 1
-        if stone == 0:
-            new_stones.append(1)
-        # Rule 2
-        elif (math.floor(np.log10(stone) + 1) % 2) == 0:
-            storage_stone = str(stone)
-            stone1, stone2 = [int(storage_stone[:int(len(storage_stone) / 2)]), int(storage_stone[int(len(storage_stone) / 2):])]
-            new_stones.extend([stone1, stone2])
-        else:
-            new_stones.append(stone*2024)
-    return new_stones
 
 def main():
-    stones = load_file("input.txt")
-    print(stones)
-    for i in range(0, 6):
-        stones = blink(stones)
-    # print(stones)
-    print(len(stones))
+    fp = "input.txt"
+    # fp = "ti1.txt"
+    num_blinks = 75
+    quantum_pillars = PillarLine(fp)
+    print(quantum_pillars.stones)
+    final_num = quantum_pillars.rapid_blink(num_blinks)
+    print("The final count is:")
+    print(final_num)
 
 
 if __name__ == "__main__":
