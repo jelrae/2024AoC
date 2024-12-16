d015aec33028d0849ea72729eedbb67975490278
Jordan Earle <43170680+jelrae@users.noreply.github.com>
2024-12-16 09:27:37 +0100

    test 0 and 1 passed

--- 12day/main12.py
+++ 12day/main12.py
@@ -116,7 +116,7 @@ class GardenPlot:
 
 def main():
     # fp = "input.txt"
-    fp = "ti0.txt"
+    fp = "ti1.txt"
     garden = GardenPlot(fp)
     garden.print_garden()
     garden.print_regions_map()
