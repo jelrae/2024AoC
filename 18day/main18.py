30fd90aa7e76b5b2064245a76f91daa0efd6c16f
Jordan Earle <43170680+jelrae@users.noreply.github.com>
2024-12-18 11:29:50 +0100

    part1 works

--- 18day/main18.py
+++ 18day/main18.py
@@ -1,6 +1,6 @@
 import numpy as np
 import itertools
-
+from heapq import heapify, heappop, heappush
 
 def load_data(fp):
     data = []
@@ -15,42 +15,77 @@ def load_data(fp):
 
 class GridSpace:
     def __init__(self, size, db):
+        self.gridsize = size + 1
         self.start = tuple([0, 0])
         self.end = tuple([size, size])
-        self.safe_grid = np.ones([size + 1, size + 1])
-        # self.unsafe_grid = np.zeros([size+1, size+1])
+        self.safe_grid = np.ones([self.gridsize, self.gridsize])
         self.dropped_bits = db
         self.tiles = {}
+        self.loc_to_id = {}
+        self.id_to_loc = {}
         self.init_tiles()
+        self.cur_time = 0
 
     def init_tiles(self):
+        id = 0
         for i in range(self.end[0] + 1):
             for j in range(self.end[0] + 1):
-                self.tiles[tuple([i, j])] = Tile([i, j], self.end[0] + 1)
+                self.tiles[tuple([i, j])] = Tile(id,[i, j], self.end[0] + 1)
+                self.loc_to_id[tuple([i, j])] = id
+                self.id_to_loc[id] = tuple([i, j])
+                id += 1
 
-    def knockout_panel(self, step):
-        panel = tuple(self.dropped_bits[step])
+    def knockout_panel(self):
+        panel = tuple(self.dropped_bits[self.cur_time])
         self.safe_grid[panel] = 0
         ns = self.tiles[tuple(panel)].neighbours
         for n in ns:
             self.tiles[tuple(n)].remove_neighbour(panel)
         del self.tiles[tuple(panel)]
+        self.cur_time += 1
 
     def jump_to_state(self, end_step):
         if end_step == -1:
             end_step = len(self.dropped_bits)
         for i in range(0, end_step):
-            self.knockout_panel(i)
-        print(self.safe_grid)
-        print(1 - self.safe_grid)
+            self.knockout_panel()
+        # print(self.safe_grid)
+        # print(1 - self.safe_grid)
 
     def find_shortest_path(self):
+        self.distance = {p: np.inf for p in self.tiles.keys()}
+        self.paths = {p: [] for p in self.tiles.keys()}
+        ten_dist = 0
+        visited_locs = set()
+        self.distance[self.start] = 0
+        clc = self.loc_to_id[self.start]
+        pos_locs = [(0, clc)]
+        heapify(pos_locs)
+        while pos_locs:
+            cd, clc = heappop(pos_locs)
+            if clc in visited_locs:
+                continue
+            visited_locs.add(clc)
+            try:
+                for neigh in self.tiles[self.id_to_loc[clc]].neighbours:
+                    ten_dist = cd + 1
+                    if ten_dist < self.distance[neigh]:
+                        self.distance[neigh] = ten_dist
+                        heappush(pos_locs, (ten_dist, self.loc_to_id[neigh]))
+            except:
+                breakpoint()
 
-        perm_set = itertools.permutations(self.safe_grid)
-
-
+    def find_first_blockage(self):
+        can_escape = True
+        while can_escape:
+            self.knockout_panel()
+            self.find_shortest_path()
+            if self.distance[tuple(self.end)] == np.inf:
+                can_escape = False
+        print("The panel which causes us to be unable to escape is {}".format(self.dropped_bits[self.cur_time-1]))
 class Tile:
-    def __init__(self, pos, max_grid):
+    def __init__(self, id, pos, max_grid):
+        self.id = id
         self.pos = pos
         self.max = max_grid
         self.neighbours = self.determine_neighbours()
@@ -65,7 +100,7 @@ class Tile:
         n = []
         for i in pos_locs:
             if i != self.pos:
-                n.append(i)
+                n.append(tuple(i))
         return n
 
     def remove_neighbour(self, n):
@@ -74,14 +109,18 @@ class Tile:
 
 
 def main():
-    # fp = 'input.txt'
+    fp = 'input.txt'
     gs = 70
-    fp = 'ti0.txt'
-    gs = 6
+    es = 1024
+    # fp = 'ti0.txt'
+    # gs = 6
+    # es = 12
     data = load_data(fp)
     grid = GridSpace(gs, data)
-    grid.jump_to_state(12)
-    breakpoint()
+    grid.jump_to_state(es)
+    grid.find_shortest_path()
+    print("The shortest path to {0} was {1}".format(grid.end, grid.distance[tuple(grid.end)]))
+    grid.find_first_blockage()
 
 
 if __name__ == '__main__':
